c805228e6237677458773bd8bbf0838af3af35aa
bugsbunnyla <slnk98@yahoo.com>
2026-07-29 13:45:31 -0400

    theatre version

--- assets/js/app.js
+++ assets/js/app.js
@@ -687,6 +687,7 @@ class ChandamamaApp {
   init() {
     this.bindEvents();
     this.renderLanguageCards();
+    this.setLanguage('te');
   }
 
   bindEvents() {
@@ -930,7 +931,7 @@ class ChandamamaApp {
   downloadScript() {
     if (!this.currentStory) return;
     const data = { title: this.currentStory.title, language: this.lang, lines: this.currentStory.lines };
-    const blob = new Blob([JSON.stringify(data, null, 2)], { type: 'application/json' };
+    const blob = new Blob([JSON.stringify(data, null, 2)], { type: 'application/json' });
     const url = URL.createObjectURL(blob);
     const a = document.createElement('a');
     a.href = url;
